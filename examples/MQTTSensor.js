763cac01da468f480f26d4097a05c204aff8eaf9
taejin park <bmtjpark@ntels.com>
2014-08-19 11:09:59 +0900

    #5

--- examples/MQTTSensor.js
+++ examples/MQTTSensor.js
@@ -1,34 +1,44 @@
 
 var aotrpi = require('../lib/aot-rpi');
-var gpio = require("pi-gpio");
+var sensor = require('ds18x20');
 var DEVICE_ID = "D43909863659";
 var DEVICE_KEY = "22b342d1382c4b0b";
 var NODE_ID = "1";
-var gpioPin = 7;
+
+var isLoaded = sensor.isDriverLoaded();
+console.log(isLoaded);
+
+
+try {
+    sensor.loadDriver();
+    console.log('driver is loaded');
+} catch (err) {
+    console.log('something went wrong loading the driver:', err)
+}
+
 
 setInterval(function() {	
-    gpio.read(gpioPin, function(err, value) {
-        if(err) {
-            throw err;
-        }
-        var pub_client = aotrpi.getPubClient(DEVICE_ID,DEVICE_KEY);
-        console.log(value);
-        var content = {
-            "message_type" : "node.content",
-            "device_id" : DEVICE_ID,
-            "device_key" : DEVICE_KEY,
-            "node_id" : NODE_ID,
-            "content_value" : value+""
-        };
-        console.log(JSON.stringify(content)); 
-        pub_client.publish('/'+DEVICE_ID+'/publish', JSON.stringify(content));
-        pub_client.on('error', function(err) {
-            //client.stream.end();
-            console.log('error!');
-        });
-        pub_client.end();
-        console.log("success !!");  
+   
+    var pub_client = aotrpi.getPubClient(DEVICE_ID,DEVICE_KEY);
+    var value = sensor.get('28-000004ccd5d4');
+    console.log(value);
+    
+    var content = {
+        "message_type" : "node.content",
+        "device_id" : DEVICE_ID,
+        "device_key" : DEVICE_KEY,
+        "node_id" : NODE_ID,
+        "content_value" : value+""
+    };
+    console.log(JSON.stringify(content)); 
+    pub_client.publish('/'+DEVICE_ID+'/publish', JSON.stringify(content));
+    pub_client.on('error', function(err) {
+        //client.stream.end();
+        console.log('error!');
     });
+    pub_client.end();
+    console.log("success !!");  
+    
 
 }, 5*1000);
 
--- examples/RESTSensor.js
+++ examples/RESTSensor.js
@@ -1,21 +1,30 @@
 
 var aotrpi = require('../lib/aot-rpi');
-//var gpio = require("pi-gpio");
-var gpio = require('rpi-gpio');
+var sensor = require('ds18x20');
 var AUTHORIZATION = "Basic RDI0MTQ3NDgwMjQ5OjBjNmVhNjk1ZmZjZDQwOGQ=";
 var DEVICE_ID = "D24147480249";
 var NODE_ID = "1";
-var gpioPin = 7;
+
+
+var isLoaded = sensor.isDriverLoaded();
+console.log(isLoaded);
+
+try {
+    sensor.loadDriver();
+    console.log('driver is loaded');
+} catch (err) {
+    console.log('something went wrong loading the driver:', err)
+}
 
 setInterval(function() {
+	    
+	var value = sensor.get('28-000004ccd5d4');
+    console.log(value);
+	var content =[{
+		"content_value" : value+""
+	}];
+	console.log(content);
+	aotrpi.post(AUTHORIZATION,DEVICE_ID,NODE_ID,content);
 	
-	gpio.read(gpioPin, function(err, value) {
-	    if(err) throw err;
-		var content =[{
-    		"content_value" : value+""
-		}];
-		console.log(content);
-		aotrpi.post(AUTHORIZATION,DEVICE_ID,NODE_ID,content);
-	});	
 	
 }, 5*1000);
